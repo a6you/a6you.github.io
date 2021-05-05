9566407ff33a203611372fcef8ac1b41063b699c
a6you <austiny365@gmail.com>
2021-05-05 18:09:57 -0400

    Redesigned layout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-$(".firstP").mouseover(function() {
-    $(this).css("color", "black");
-});
-
-$(".firstP").mouseout(function() {
-    $(this).css("color", "white");
-});
+function pixelFontToggle() {
+    if (document.getElementsByTagName("div")[0].style.fontFamily != "VT323") {
+        $('div').css('fontFamily', 'VT323');
+        $('div').css('font-size', '20px');
+    } else {
+        $('div').css('fontFamily', '');
+        $('div').css('font-size', '');
+    }
+}
